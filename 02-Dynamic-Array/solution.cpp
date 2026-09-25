394be87d0162da66f4416718403699d6421766b4
Poonam kumari <poonam100k12345@gmail.com>
2026-09-25 09:02:54 +0530

    Add dynamicArray function for query processing
    Implement dynamic array functionality to handle queries.

--- 02-Dynamic-Array/solution.cpp
+++ 02-Dynamic-Array/solution.cpp
@@ -1,1 +1,24 @@
 
+vector<int> dynamicArray(int n, vector<vector<int>> queries) {
+    vector<vector<int>> arr(n);
+    vector<int> answer;
+    int lastAnswer = 0;
+
+    for (auto query : queries) {
+        int type = query[0];
+        int x = query[1];
+        int y = query[2];
+
+        int idx = (x ^ lastAnswer) % n;
+
+        if (type == 1) {
+            arr[idx].push_back(y);
+        }
+        else if (type == 2) {
+            lastAnswer = arr[idx][y % arr[idx].size()];
+            answer.push_back(lastAnswer);
+        }
+    }
+
+    return answer;
+}
