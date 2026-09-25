e53392355550d998d54bee1a77a2b58360777947
Poonam kumari <poonam100k12345@gmail.com>
2026-09-25 08:55:18 +0530

    Add diagonalDifference function
    Implement function to calculate diagonal difference of a square matrix.

--- 01-Diagonal-Difference/solution.cpp
+++ 01-Diagonal-Difference/solution.cpp
@@ -1,1 +1,14 @@
 
+int diagonalDifference(vector<vector<int>> arr) {
+    int leftSum = 0;
+    int rightSum = 0;
+
+    int n = arr.size();
+
+    for (int i = 0; i < n; i++) {
+        leftSum += arr[i][i];
+        rightSum += arr[i][n - 1 - i];
+    }
+
+    return abs(leftSum - rightSum);
+}
