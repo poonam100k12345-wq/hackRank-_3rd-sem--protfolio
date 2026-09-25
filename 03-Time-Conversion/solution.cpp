ada3f59702da26092a91d59d13f3c78ab477ae9e
Poonam kumari <poonam100k12345@gmail.com>
2026-09-25 09:06:41 +0530

    Add timeConversion function for 12-hour format
    Implement time conversion from 12-hour to 24-hour format.

--- 03-Time-Conversion/solution.cpp
+++ 03-Time-Conversion/solution.cpp
@@ -1,1 +1,21 @@
+string timeConversion(string s) {
+    int hour = stoi(s.substr(0, 2));
 
+    if (s.substr(8, 2) == "AM") {
+        if (hour == 12)
+            hour = 0;
+    }
+    else {
+        if (hour != 12)
+            hour += 12;
+    }
+
+    string result = to_string(hour);
+
+    if (hour < 10)
+        result = "0" + result;
+
+    result += s.substr(2, 6);
+
+    return result;
+}
