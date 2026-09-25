89ecc5487d41df4280095b3b4f4fe3629059915e
Poonam kumari <poonam100k12345@gmail.com>
2026-09-25 09:15:05 +0530

    Update solution.cpp

--- 05-Sparse-Arrays/solution.cpp
+++ 05-Sparse-Arrays/solution.cpp
@@ -1,1 +1,17 @@
+vector<int> matchingStrings(vector<string> stringList, vector<string> queries) {
+    vector<int> answer;
 
+    for (string q : queries) {
+        int count = 0;
+
+        for (string s : stringList) {
+            if (s == q) {
+                count++;
+            }
+        }
+
+        answer.push_back(count);
+    }
+
+    return answer;
+}
