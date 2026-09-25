68b9963e247e4b2cad32cf8aa125c9bd8de617c4
Poonam kumari <poonam100k12345@gmail.com>
2026-09-25 09:10:37 +0530

    Implement compareTriplets function for scoring

--- 04-Compare-the-Triplets/solution.cpp
+++ 04-Compare-the-Triplets/solution.cpp
@@ -1,1 +1,16 @@
 
+vector<int> compareTriplets(vector<int> a, vector<int> b) {
+    int alice = 0;
+    int bob = 0;
+
+    for (int i = 0; i < 3; i++) {
+        if (a[i] > b[i]) {
+            alice++;
+        }
+        else if (a[i] < b[i]) {
+            bob++;
+        }
+    }
+
+    return {alice, bob};
+}
